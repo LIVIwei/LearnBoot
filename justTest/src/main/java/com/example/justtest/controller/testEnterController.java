d69d2621d0329e663b96673526aff1a05cd5fded
LIVIwei <1210353887@qq.com>
2023-03-16 15:41:07 +0800

    1

---
+++ justTest/src/main/java/com/example/justtest/controller/testEnterController.java
@@ -0,0 +1,15 @@
+package com.example.justtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: LIVI
+ * @Time: 2023/03/16  15:27
+ * @Description: 测试入口
+ */
+@Controller
+@RequestMapping("/justTest")
+public class testEnterController {
+
+}
